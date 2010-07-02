4c0327b36a2578eb9501a0a572b0f35db3b810ba
making <makingx@gmail.com>
2010-07-03 03:47:19 +0900

    update GAE SDK to 1.3.5

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :repositories {"maven.seasar.org" "http://maven.seasar.org/maven2"}
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]
-                 [com.google.appengine/appengine-api-1.0-sdk "1.3.4"]
-                 [com.google.appengine/appengine-testing "1.3.4"]
-                 [com.google.appengine/appengine-api-labs "1.3.4"]
-                 [com.google.appengine/appengine-api-stubs "1.3.4"]]
-  :dev-dependencies [;; [com.google.appengine/appengine-local-runtime "1.3.4"]
+                 [com.google.appengine/appengine-api-1.0-sdk "1.3.5"]
+                 [com.google.appengine/appengine-testing "1.3.5"]
+                 [com.google.appengine/appengine-api-labs "1.3.5"]
+                 [com.google.appengine/appengine-api-stubs "1.3.5"]]
+  :dev-dependencies [;; [com.google.appengine/appengine-local-runtime "1.3.5"]
                      ;; [leiningen/lein-swank "1.1.0"]
                      [lein-clojars "0.5.0-SNAPSHOT"]] 
   :namespaces [am.ik.clj-gae-testing.test-utils])
